ceb67894805bde31a85f3fc540109403b532e253
MelisaVera <a01209183@itesm.mx>
2020-05-18 22:10:09 -0500

    FPS

---
+++ ProyEgipto/Assets/Scripts/SeleccionarPiedra.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+public class SeleccionarPiedra : SeleccionarPiedra
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	Debug.Log ("se dio clic sobre: "+ gameObject.game);
+       LogicaJuego.agregarPiedra(gameObject.name);
+    }
+}
+*/
